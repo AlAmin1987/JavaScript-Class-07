cf28180f2be7cdaf414b287957326fca53f2ef63
Md.AlAmin <alamin.dreams2021@gmail.com>
2022-12-31 00:09:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,155 @@
+// JavaScript Object
+
+// at first example of Javascript Array
+
+// const newArray = ["Shakhawat", "AlAmin", 35, "Web Developer"];
+// console.log(newArray);
+// newArray.push("Gazipur");
+// console.log(newArray);
+
+// console.log(newArray[2]);
+
+// console.log(newArray[1]);
+
+//  Example of javascripts Object
+
+// const newobject = {
+//   firstName: "Shakhawat",
+//   lastName: "AlAmin",
+//   age: 35,
+//   Place: "Gazipur",
+//   job: "Web Developer",
+// };
+
+// console.log(newobject);
+// console.log(newobject.age);
+// console.log(newobject.firstName);
+// console.log(newobject["lastName"]);
+// console.log(newobject.job);
+
+// added newitem
+
+// newobject.birthYeatr = 1987;
+// console.log(newobject);
+// console.log(newobject.birthYear);
+
+// Another Example
+
+// newobject["birthPlace"] = "Jamalpur";
+// console.log(newobject["birthPlace"]);
+
+// Another Object Example
+
+// const newobject = {
+//   firstName: "Shakhawat",
+//   lastName: "AlAmin",
+//   age: 35,
+//   Place: "Gazipur",
+//   job: "Web Developer",
+//   ageCalculation: function (birthYear) {
+//     return 2022 - birthYear;
+//   },
+// };
+
+// console.log(newobject.ageCalculation(1975));
+
+// JavaScript Loop
+
+// for loop
+
+// console.log("workinNumber 1");
+// console.log("workinNumber 2");
+// console.log("workinNumber 3");
+// console.log("workinNumber 4");
+// console.log("workinNumber 5");
+// console.log("workinNumber 6");
+// console.log("workinNumber 7");
+// console.log("workinNumber 8");
+// console.log("workinNumber 9");
+// console.log("workinNumber 10");
+// console.log("workinNumber 11");
+
+// for (let i = 1; i <= 8; i++) {
+//   console.log(`workingNumber ${i}`);
+// }
+
+// for (i = 1; i <= 300; i++) {
+//   console.log(`Placement ${i}`);
+// }
+
+// for (i = 0; i <= 45; i++) {
+//   console.log(`tracking Number${i}`);
+// }
+
+// for (i = 5; i <= 12; i++) {
+//   console.log(`Working Number ${i}`);
+// }
+
+// While Loop
+
+// i = 0;
+// while (i <= 10) {
+//   console.log(`Working Number ${i}`);
+//   i++;
+// }
+
+// loop  an Array
+
+// const newArray = ["Ashik 1", "Ashik 2", "Ashik 3", "Ashik 4", "Ashik 5"];
+
+// for (i = 0; i <= 4; i++) {
+//   console.log(`Ashik ${i}`);
+// }
+
+// Another Example
+
+// const newAnotherArray = [
+//   "Data 1",
+//   "Data 2",
+//   "Data 3",
+//   "Data 4",
+//   "Data 5",
+//   "Data 6",
+// ];
+
+// for (i = 0; i <= 5; i++) {
+//   console.log(`Data${i}`);
+// }
+
+// Backward position Run
+
+// for (i = newAnotherArray.length - 1; i >= 0; i--) {
+//   console.log(`Data ${i}`);
+// }
+
+// loop Break
+
+// const newAnotherArray = [
+//   "Data 1",
+//   "Data 2",
+//   "Data 3",
+//   "Data 4",
+//   "Data 5",
+//   "Data 6",
+// ];
+
+// for (i = 0; i <= 5; i++) {
+//   if (i == 5) break;
+//   console.log(`Data${i}`);
+// }
+
+// Loop Continue
+
+// for (i = 0; i <= 5; i++) {
+//   if (i == 2) continue;
+//   console.log(`Data${i}`);
+// }
+
+// indexof
+
+// console.log(newAnotherArray.indexOf("Data 5"));
+// console.log(newAnotherArray.indexOf("Data 3"));
+
+// includes
+
+// console.log(newAnotherArray.includes("Data 6"));
